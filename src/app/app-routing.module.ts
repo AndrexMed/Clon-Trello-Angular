893e0208eda4573fc48e7bd5b1f0076e66585285
AndrexMed <giovanniandrex@gmail.com>
2023-10-10 23:50:04 -0500

    Creando OverLay

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
+import { BoardsComponent } from './pages/boards/boards.component';
 
 const routes: Routes = [
   {
+    path: "",
+    redirectTo: "login",
+    pathMatch: "full",
+  },
+  {
     path: "login",
     component: LoginComponent
+  },
+  {
+    path: "boards",
+    component: BoardsComponent
   }
 ];
 
