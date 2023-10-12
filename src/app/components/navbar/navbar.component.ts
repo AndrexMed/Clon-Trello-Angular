2a773a1f602b77c810ec03eee97f25d8dc74744f
AndrexMed <giovanniandrex@gmail.com>
2023-10-11 19:42:43 -0500

    Icons - FontAwesome

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
 
+//Icons
+import { faBell, faInfoCircle } from '@fortawesome/free-solid-svg-icons';
+
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html'
 })
 export class NavbarComponent {
+
+  faBell = faBell
+  faInfoCircle = faInfoCircle
+
   isOpen = false;
   isOpenBody = false;
 }
--- src/app/pages/boards/boards.component.ts
+++ src/app/pages/boards/boards.component.ts
@@ -1,9 +1,17 @@
 import { Component } from '@angular/core';
 
+import { faBox, faWaveSquare, faClock } from '@fortawesome/free-solid-svg-icons';
+import { faTrello } from '@fortawesome/free-brands-svg-icons';
+
 @Component({
   selector: 'app-boards',
   templateUrl: './boards.component.html'
 })
 export class BoardsComponent {
+
+  faBox = faBox
+  faWaveSquare = faWaveSquare
+  faClock = faClock
+  faTrello = faTrello
 
 }
